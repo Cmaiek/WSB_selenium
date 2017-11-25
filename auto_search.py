6649d84691e531a1db82847bc5f6f55ed02fc82d
wojciech11 <wojciechbarczynski.wsb+1@gmail.com>
2017-11-25 19:38:32 +0000

    Simple example

---
+++ auto_search.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.get("https://encrypted.google.com")
+assert "Google" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("wsb tester wroclaw")
+elem.send_keys(Keys.ENTER)
+
+wait = WebDriverWait(driver, 10)
+
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "res")))
+
+driver.quit()
